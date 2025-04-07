e847e18bccc48b4514460be39ee56b747010a1af
GhostEasyX <jhonoliveirapsh@gmail.com>
2025-04-07 01:10:33 -0300

    funcionou

--- jxj.py
+++ jxj.py
@@ -1,11 +1,7 @@
 import pyautogui
 from time import sleep
-ganhos = 0
 
-#############################
 def InfinitamenteBuscandoImagem(imagem, tempo=2, fidelidade=0.8, tempo_inicial=2):
-    #o intuito dessa função é buscar por uma imagem específica até encontrar.
-    #caso não encontre a imagem, a função continuará buscando infinitamente!
     while True:
         try:
             IMAGEM = pyautogui.locateCenterOnScreen(f'D:\\jxjproject\\imagens\\{imagem}.png', confidence=fidelidade)
@@ -19,7 +15,6 @@ def InfinitamenteBuscandoImagem(imagem, tempo=2, fidelidade=0.8, tempo_inicial=2
 
 
 def verificacao_inicial(tempo=2, fidelidade=0.8):
-    global ganhos
     for __ in range(10):
         sleep(1)
         try:
@@ -30,22 +25,18 @@ def verificacao_inicial(tempo=2, fidelidade=0.8):
             pyautogui.click(IMAGE2)
             sleep(1)
             print('FUNCINOU!!!!!!!!!')
-            ganhos +=1
-            print(f'Tive um total de {ganhos} ganhos.')
             sleep(tempo)
             break
         except pyautogui.ImageNotFoundException:
             print('não encontrei')
 
 def toque_para_fechar():
-    controle = True
-    while controle:
+    while True:
             try:
                 FIRST_IMAGE = pyautogui.locateCenterOnScreen(f'D:\\jxjproject\\imagens\\toque_para_fechar_mais_seguro.png', confidence=0.8)
                 sleep(2)
                 pyautogui.click(FIRST_IMAGE)
                 print('FUNCINOU !!!!!!!!!')
-                controle = False
                 return 0
             except pyautogui.ImageNotFoundException:
                 print('não encontrei 1')
@@ -55,7 +46,6 @@ def toque_para_fechar():
                     sleep(2)
                     pyautogui.click(SECOND_IMAGE)
                     print('FUNCINOU !!!!!!!!!')
-                    controle = False
                     return 1
                 except pyautogui.ImageNotFoundException:
                     print('não encontrei 2')
@@ -67,4 +57,3 @@ for Teste in range(50):
     InfinitamenteBuscandoImagem('comecar_partida')
     toque_para_fechar()
     print(f'joguei {Teste} partidas!')
-    print(f'Tive um total de {ganhos}ganhos.')
